44017e39f0c98856b3beb14fcc916c96a8f5f737
snakeyeah <gurshandhaliwal509@outlook.com>
2025-03-10 11:52:09 +0000

    yeahhh

--- menu_wrapper.py
+++ menu_wrapper.py
@@ -1,18 +1,18 @@
 import pygame
 from screen import Screen
-from menu_screen import MenuScreen
+from menu_screen import MenuScreen  # to maintain compatibility with game screen
 
 class MenuScreenWrapper(Screen):
-    def __init__(self, surface):
+    def __init__(self, surface):   # uses the parent class
         super().__init__(surface)
         self.menu = MenuScreen(surface)
     
     def handle_events(self, event):
-        return self.menu.handle_events(event)
+        return self.menu.handle_events(event)    #event handler
     
     def update(self, game_data=None):
-        mouse = pygame.mouse.get_pos()
+        mouse = pygame.mouse.get_pos()    #gets the mouse position and passes it
         self.menu.update(mouse)
     
     def draw(self):
-        self.menu.draw()
+        self.menu.draw()   #calls the draw method
