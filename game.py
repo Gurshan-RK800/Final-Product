f89a6c1f4c47690ba7a8d30ba82bf05d84ea62fa
snakeyeah <gurshandhaliwal509@outlook.com>
2025-03-10 11:39:23 +0000

    yeahhh

--- game.py
+++ game.py
@@ -1,48 +1,49 @@
 import pygame
 import sys
 from menu_wrapper import MenuScreenWrapper
-from playing_screen import PlayingScreen
+from playing_screen import PlayingScreen     # Imports from other files to be used
 from help_screen import HelpScreen
 
-class Game:
+class Game:     # handles the main game window + the state
     def __init__(self, title="Dusk Dashers", width=800, height=600, bg_color=(0, 0, 0)):
         pygame.init()
         self.window = pygame.display.set_mode((width, height))
         pygame.display.set_caption(title)
         self.background = bg_color
-        self.clock = pygame.time.Clock()
-        self.running = True
+        self.clock = pygame.time.Clock()     # control the frame frate
+        self.running = True         # keeps track if the game is running
         
         self.MENU = "menu"
-        self.GAME = "playing" 
+        self.GAME = "playing"        # defines the game states here
         self.HELP = "help"
-        
+
         self.current = self.MENU
         
         self.game_screens = {
-            self.MENU: MenuScreenWrapper(self.window),
-            self.GAME: PlayingScreen(self.window),
-            self.HELP: HelpScreen(self.window)
+            self.MENU: MenuScreenWrapper(self.window),      # game player screen
+            self.GAME: PlayingScreen(self.window),       # main meny screen
+            self.HELP: HelpScreen(self.window)       #instructions here
         }
         
     def handle_input(self):
-      
+         #essentiallt the basic game loop
         for event in pygame.event.get():
             
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:  #Pressing 'x' would close the game
                 self.running = False
                 return
             
            
             next_screen = self.game_screens[self.current].handle_events(event)
-            
+
+           # Ensures the game only transitions to actually valid screens.
            
             if next_screen:
                 if next_screen in self.game_screens:
                    
                     self.current = next_screen
                 elif next_screen == "exit":
-                    print("Thanks for playing!")
+                    print("Thanks for playing!")  # if returned screen is 'exit' adds a message
                     self.running = False
             
     def update(self):
